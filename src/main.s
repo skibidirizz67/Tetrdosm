7315737d9e1cacaafc5ac7e1a0d5e2c8a8a33618
skibidirizz67 <gitskibidirizz@proton.me>
2026-01-27 20:34:52 +0200

    finally working counter im so dumb

--- src/main.s
+++ src/main.s
@@ -5,23 +5,25 @@ _start:
     li a4, 1024 # max counter value
     li a5, 10 # digit count
     li a2, 2 # write len
-    li s1, 3 # biggest digit offset
+    li s0, 8 # string len
+    li s1, 6 # offset
 
 while:
     la a1, digit
+    add t1, a1, s0 # i
+    addi t1, t1, -2
     add a1, a1, s1
-    mv t1, a1 # i
     mv t2, a3 # tmp
     jal b2ds
     jal write
     addi a3, a3, 1
     beq a3, a4, exit
-    lb t0, 0(a1)
-    addi t0, t0, -'0'
-    bnez t0, skip
-    addi s1, s1, -1
+    lb t1, 0(a1)
+    addi t1, t1, -'9'
+    bnez t1, skip
     addi a2, a2, 1
-    skip:
+    addi s1, s1, -1
+skip:
     jal sleep
     j while
 
@@ -42,4 +44,4 @@ b2ds:
     ret
 
     .section .data
-digit: .ascii "0000\n"
+digit: .ascii "0000000\n"
