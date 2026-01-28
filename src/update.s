2529b74fa5fdef85ddb255eba4936af9de01c15a
skibidirizz67 <gitskibidirizz@proton.me>
2026-01-28 04:42:53 +0200

    static block collision check

--- src/update.s
+++ src/update.s
@@ -2,7 +2,7 @@
     .global update
 update:
     la s0, playfield
-    li s1, 11*20-1
+    li s1, 11*20-1 # TODO: replace counter s4 with s1 + s0
     li s6, 11*19+1
     li s2, '#'
     li s3, '.'
@@ -12,17 +12,27 @@ update:
     add s4, s0, s1
     lb t1, 0(s4)
     bne t1, s2, 1f
-    blt s1, s6, 2f
+    lb t2, 11(s4)
+    beq t2, s5, 2f
+    blt s1, s6, 1f
+    2:
     mv t0, ra
     jal bake
     jal spawn
     mv ra, t0
     ret
-    2:
+    1:
+    bnez s1, while
+    li s1, 11*20-1
+    rwhile:
+    addi s1, s1, -1
+    add s4, s0, s1
+    lb t1, 0(s4)
+    bne t1, s2, 1f
     sb s2, 11(s4)
     sb s3, 0(s4)
     1:
-    bnez s1, while
+    bnez s1, rwhile
     ret
 
 bake:
